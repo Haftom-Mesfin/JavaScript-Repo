b5f4c732bec95ca6de0e35974889522ba3f3fc06
Haftom-Mesfin <sadtime1906@gmail.com>
2024-10-15 21:21:20 -0500

    additional execises

--- exercise/index.js
+++ exercise/index.js
@@ -11,3 +11,37 @@ console.log("Second number is : " + secondtCard)
 let sum = firstCard + secondtCard
 
 console.log("Sum is : " + sum)
+
+
+//-------------------------------------
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age >= 21) {
+    console.log("Welcome! You can enter the club!")
+} else {
+    console.log("Sorry, You can not enter the club!")
+}
+
+//--------------------------------------
+
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let personAge = 100
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (personAge < 100) {
+    console.log("Not elegible")
+} else if (personAge === 100) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
